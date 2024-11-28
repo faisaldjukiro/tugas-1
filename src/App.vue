172cdce21a362516d3172ebe00691d8654aa55e4
unknown <faisaldjukiro98@gmail.com>
2024-11-28 19:34:52 +0800

    pertama commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import Tugaskedua from './components/Tugaskedua.vue'
+export default {
+  name: 'App',
+  components: {
+    Tugaskedua
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <Tugaskedua />
+  </div>
+</template>
---
+++ src/components/Tugaskedua.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <p class="orange-text">
+            <b>Selamat malam tuton. </b> Ini adalah tugas kedua saya
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Tugaskedua'
+};
+</script>
